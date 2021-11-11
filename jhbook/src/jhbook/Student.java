6dcbfbb9b37234bef60fa716fb650e5582cb4644
hadora98kim <4082370@naver.com>
2021-11-11 15:56:06 +0900

    server연결

---
+++ jhbook/src/jhbook/Student.java
@@ -0,0 +1,35 @@
+package jhbook;
+
+
+public class Student {
+	private String no;
+	private String name;
+	private String depart;
+	private String memo;
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepart() {
+		return depart;
+	}
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+}
